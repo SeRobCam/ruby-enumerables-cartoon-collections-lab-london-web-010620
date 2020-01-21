9457533a6b3845f65d5e8e943fd21d773865a880
Sergio Robles <sroblesc@gmail.com>
2020-01-21 11:50:00 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,14 +1,24 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(array)
+  puts array
 end
+
+
+
 
 def summon_captain_planet# code an argument here
   # Your code here
 end
 
+
+
+
+
 def long_planeteer_calls# code an argument here
   # Your code here
 end
+
+
+
 
 def find_the_cheese# code an argument here
   # the array below is here to help
